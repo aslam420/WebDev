90452d6f54f694ec69442764bf1a8d40d21e3cb1
Mohammad Aslam <aslampaasa420@gmail.com>
2023-09-01 02:26:14 +0530

    ReactJS

---
+++ PepCoding/React/todo2/src/index.js
@@ -0,0 +1,89 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+
+class AddTask extends React.Component{
+    render(){
+        return(
+            <form>
+                <input type="text"/>
+                <input type="button" value="Add Task"/>
+            </form>
+        )
+    }
+}
+
+class TaskList extends React.Component {
+    constructor(props){
+        super();
+    }
+    render(){
+        let list = [];
+        for(let i = 0; i < this.props.tasks.length; i++){
+            let task = this.props.tasks[i];
+            let listItem = (
+                <li key={i}>
+                    <span>{task.desc}</span>
+                    <button>{this.props.purpose == "Todo" ? "do" : "undo"}</button>
+                </li>);
+                list.push(listItem);
+        }
+
+        return(
+                <div className={this.props.forStyling}>
+                    <div>{this.props.purpose}</div>
+                    <ul>
+                        {list}
+                    </ul>
+                </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props){
+        super();
+
+        this.state = {
+            tasks: [{
+                desc: 'Switch off light',
+                isFinished: false
+            },
+            {
+                desc: 'Turn on fan',
+                isFinished: true
+            },
+            {
+                desc: 'Make tea',
+                isFinished: false
+            },
+            {
+                desc: 'Make dinner',
+                isFinished: true
+            }]
+        }
+    }
+
+
+  render() {
+    let tasks = this.state.tasks;
+    let todoTasks = tasks.filter(t => t.isFinished == false);
+    let doneTasks = tasks.filter(t => t.isFinished == true);
+
+    return (
+    <>
+        <div className="add-task">
+            <AddTask />
+        </div>
+        
+        <div className='task-lists'>
+            <TaskList tasks={todoTasks} purpose="Todo" forStyling="todo"/>
+            <TaskList tasks={doneTasks} purpose="Finished" forStyling="finished"/>
+        </div>
+
+    </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
